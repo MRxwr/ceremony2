91212f346ec31f6f67c3390b0d916270cbc98f60
MRxwr <nasserhatab@gmail.com>
2025-06-28 23:31:20 +0300

    Update head-section.php

--- templates/theme1/head-section.php
+++ templates/theme1/head-section.php
@@ -1,4 +1,4 @@
 <div class="card-header-section">
-    <h1 class="couple-names"><?php echo direction($event["enTitle"],$event["arTitle"]); ?></h1>
+    <h1 class="couple-names"><?php echo $event["title"]; ?></h1>
     <p class="wedding-date"><?php echo $event["eventDate"]; ?></p>
 </div>
