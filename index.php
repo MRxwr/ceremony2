d2f0742d5fef4d28fe9f4f0a2dcae3d78534b6c6
MRxwr <nasserhatab@gmail.com>
2025-06-28 23:30:31 +0300

    save

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once("dashboard/includes/config.php");
 require_once("dashboard/includes/functions.php");
 if( isset($_REQUEST["systemCode"]) && !empty($_REQUEST["systemCode"]) && $event = selectDBNew("events",[$_REQUEST["systemCode"]],"`code` LIKE ? AND `hidden` = '0' AND `status` = '0'","") ){
+	$event = $event[0];
 	$systemCode = $_REQUEST["systemCode"];
 }else{
 	header("Location: default.php");die();
--- templates/theme1/head-section.php
+++ templates/theme1/head-section.php
@@ -1,4 +1,4 @@
 <div class="card-header-section">
-    <h1 class="couple-names">{{BrideName}} & {{GroomName}}</h1>
-    <p class="wedding-date">{{EventDate}}</p>
+    <h1 class="couple-names"><?php echo direction($event["enTitle"],$event["arTitle"]); ?></h1>
+    <p class="wedding-date"><?php echo $event["eventDate"]; ?></p>
 </div>
