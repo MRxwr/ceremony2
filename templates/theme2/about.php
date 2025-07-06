44e79527640a0cc5b9be0afa743c45ae4c355d77
MRxwr <nasserhatab@gmail.com>
2025-07-07 01:16:05 +0300

    save

--- templates/theme2/about.php
+++
@@ -1,7 +0,0 @@
-<div class="content-panel" id="about-panel">
-    <h3 class="panel-title"><?php echo direction("Event Highlights","مواضيع الحفل") ?></h3>
-    
-    <div class="details-content">
-        <?php echo $event["details"] ?>
-    </div>
-</div>
