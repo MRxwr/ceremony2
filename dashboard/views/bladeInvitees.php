41e088f23045c8d7080d46875a8f4fafbf181736
MRxwr <nasserhatab@gmail.com>
2025-07-07 00:25:05 +0300

    Update bladeInvitees.php

--- dashboard/views/bladeInvitees.php
+++ dashboard/views/bladeInvitees.php
@@ -25,7 +25,7 @@ if( isset($_GET["isSent"]) && !empty($_GET["isSent"]) ){
 		
 		// Construct invitee link
 		$inviteeLink = $_SERVER['HTTP_HOST'] . "/{$eventCode}?i={$inviteeCode}";
-		
+		var_dump($to, $_GET["eventId"], $inviteeLink);
 		whatsappUltraMsgImage($to, $_GET["eventId"], $inviteeLink);
 		if( updateDB('invitees',array('invitationSent'=> '1'),"`id` = '{$_GET["isSent"]}'") ){
 			header("LOCATION: ?v=Invitees&eventId={$_GET["eventId"]}");
